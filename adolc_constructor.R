966e470f3b173511c32fc930c7760c80136b3a90
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2017-01-09 08:01:44 -0600

    Add usage comments

--- adolc_constructor.R
+++ adolc_constructor.R
@@ -1,4 +1,15 @@
+# Name: adolc_createList creates a list of adoubles of
+# length count. Each element is initialized to a provided value
+# Arguments:
+#    count : positive numeric value
+#    val   : value to be initialize elements of the list
 adolc_createList <- function(count, val){
+    if(!is.numeric(count)){
+        stop("adolc_createList(): count must be numeric.")
+    }
+    if(!is.numeric(val)){
+        stop("adolc_createList(): val must be numeric.")
+    }
     if(count <= 0){
         stop("adolc_createList(): Unhandled case of count <=0")
     }
