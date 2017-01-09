448c8211a35a14ff1890aa91918857f1385c4594
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2017-01-09 07:56:05 -0600

    include constructor file

--- RFiles/init_adolc.R
+++ RFiles/init_adolc.R
@@ -2,5 +2,6 @@ dyn.load(paste("/Users/jbessac/RProjectKrishna/adol-c/ADOL-C/swig/R/adolc", .Pla
 source("src/adolc_dispatch.R")
 source("src/adolc.R")
 source("src/adolc_summary.R")
+source("src/adolc_constructor.R")
 cacheMetaData(1)
 
