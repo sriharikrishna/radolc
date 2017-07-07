e0855a29e25c11931b1792d6b0c6649aa9a26483
Sri Hari Krishna Narayanan <snarayan@mcs.anl.gov>
2017-07-07 11:07:22 -0500

    Moved file

---
+++ init_adolc.R
@@ -0,0 +1,7 @@
+dyn.load(paste("/Users/jbessac/RProjectKrishna/adol-c/ADOL-C/swig/R/adolc", .Platform$dynlib.ext, sep=""))
+source("src/adolc_dispatch.R")
+source("src/adolc.R")
+source("src/adolc_summary.R")
+source("src/adolc_constructor.R")
+cacheMetaData(1)
+
