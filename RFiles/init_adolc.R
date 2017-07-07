0dfd6b0702b27eca6bceb797bffe0e6aa31bb77d
Sri Hari Krishna Narayanan <snarayan@mcs.anl.gov>
2017-07-07 11:08:33 -0500

    Deleted

--- RFiles/init_adolc.R
+++
@@ -1,7 +0,0 @@
-dyn.load(paste("/Users/jbessac/RProjectKrishna/adol-c/ADOL-C/swig/R/adolc", .Platform$dynlib.ext, sep=""))
-source("src/adolc_dispatch.R")
-source("src/adolc.R")
-source("src/adolc_summary.R")
-source("src/adolc_constructor.R")
-cacheMetaData(1)
-
