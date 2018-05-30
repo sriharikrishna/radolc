5791bfd9e0e0f90328c4d05a072fedfc94e91017
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2018-05-30 16:02:30 -0400

    Deleted files that are moved or unnecessary

--- init_adolc.R
+++
@@ -1,7 +0,0 @@
-dyn.load(paste("ENTER_YOUR_ADOLC_SOURCE_FOLDER_HERE/ADOL-C/swig/R/adolc", .Platform$dynlib.ext, sep=""))
-source("src/adolc_dispatch.R")
-source("src/adolc.R")
-source("src/adolc_summary.R")
-source("src/adolc_constructor.R")
-cacheMetaData(1)
-
